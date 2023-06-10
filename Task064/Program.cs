78f9e4e3f7d1c38c919c4f3d98ef877633cc8352
davydova <natalya-davydova-86@mail.ru>
2023-06-10 18:37:36 +0300

    Chenged Task 064

--- Task064/Program.cs
+++ Task064/Program.cs
@@ -5,6 +5,7 @@ N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> " 8, 7, 6, 5, 4, 3, 2, 1"
 */
 
+/*
 string Rec(int start, int end)
 {
     if (start == end)
@@ -13,7 +14,28 @@ string Rec(int start, int end)
     }
     return (start + Rec(start - 1, end));
 }
-Console.WriteLine(Rec(5, 1));
+Console.WriteLine(Rec(8, 1));
+*/
+
+  Console.Write("Введите число N: ");
+  int n = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число М: ");
+  int m = Convert.ToInt32(Console.ReadLine());
+
+void Recurs (int n, int m)
+{
+    if (n >= m)
+    {
+        Console.WriteLine(n);
+        n = n - 1;
+        Recurs (n, m);
+    }
+    return;
+}
+Recurs (n, m);
+
+
+
 
 
 
