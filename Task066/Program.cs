492805396ef93003d75f6c77efce0704faaad57b
davydova <natalya-davydova-86@mail.ru>
2023-06-10 21:35:14 +0300

    Created Task066

---
+++ Task066/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задать значения M и N. Написать программу, которая найдет сумму
+натуральных элементов в промежутке от М до N.
+М = 1; N = 15 -> 120
+M = 4; N = 8 -> 30
+*/
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum(int m, int n)
+{
+if (m==n)
+return n;
+else return m + sum(m + 1, n);
+}
+Console.Write($"Сумма элементов от M до N:  {sum(m,n)}");
